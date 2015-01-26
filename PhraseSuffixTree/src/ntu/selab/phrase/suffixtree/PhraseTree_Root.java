1d561a281f41c179c3a1571c461f18ee2de751be
Terence <terenzio@gmail.com>
2015-01-26 19:41:34 +0800

    TreeConstructionAttempt1

--- PhraseSuffixTree/src/ntu/selab/phrase/suffixtree/PhraseTree_Root.java
+++ PhraseSuffixTree/src/ntu/selab/phrase/suffixtree/PhraseTree_Root.java
@@ -5,21 +5,39 @@ import java.util.Set;
 
 public class PhraseTree_Root implements Aggregator {
 	
+	private String rootName;
 	private Set<PhraseTree_Node> nodes;
 	
-//	public PhraseTree_Root() {
-//		nodes = new HashSet<PhraseTree_Node>();
-//	}
+	public PhraseTree_Root(String rootName) {		
+		this.rootName = rootName;
+		nodes = new HashSet<PhraseTree_Node>();
+	}
 	
 	protected void finalize() {
 		nodes = null;
 	}
 	
-	public void addNode(PhraseTree_Node word) {
-		nodes = new HashSet<PhraseTree_Node>();
-		nodes.add(word);
+//	public void addNode(PhraseTree_Node word) {
+//		//nodes = new HashSet<PhraseTree_Node>();
+//		this.nodes.add(word);
+//	
+//	}
 	
+	public void addNode(String word) {
+	//nodes = new HashSet<PhraseTree_Node>();
+	PhraseTree_Node newNode = new PhraseTree_Node();	
+	newNode.setWord(word);
+
 	}
 	
+	public void addNodes(String word) {
+		//nodes = new HashSet<PhraseTree_Node>();
+		nodes.add(null);
+		
+		}
+	
+	public void displayPhraseTree(String rootName) {
+		
+	}
 	
 }
--- PhraseSuffixTree/src/ntu/selab/phrase/suffixtree/test/RunTree.java
+++ PhraseSuffixTree/src/ntu/selab/phrase/suffixtree/test/RunTree.java
@@ -2,20 +2,41 @@ package ntu.selab.phrase.suffixtree.test;
 
 
 import ntu.selab.phrase.suffixtree.*;
-
 import junit.framework.TestCase;
+
 import org.junit.Test;
+
+
 import static org.junit.Assert.*;
 
 
 
 public class RunTree extends TestCase {
 
-	// MyClass is tested
-	   MyClass tester = new MyClass();
 
-	   // Tests
-	   assertEquals("10 x 0 must be 0", 0, tester.multiply(10, 0));
-	   assertEquals("0 x 10 must be 0", 0, tester.multiply(0, 10));
-	   assertEquals("0 x 0 must be 0", 0, tester.multiply(0, 0));
+		//CompactSuffixTree tree = new CompactSuffixTree(new SimpleSuffixTree("bananas$"));
+	
+	 public void testBasicTreeGeneration() {
+		 
+		 String root1 = "FirstTree";
+			
+	
+		 PhraseTree_Root phraseTree = new PhraseTree_Root(root1);
+		 	
+		
+		 phraseTree.addNode("Hello");
+		 phraseTree.addNode("How");
+		 phraseTree.addNode("Are");
+		 phraseTree.addNode("You");
+		 
+		 phraseTree.displayPhraseTree(root1);
+		 
+		 	 
+		 	
+		 
+	 }
+	 	
+	 	
+	 	
 }
+
