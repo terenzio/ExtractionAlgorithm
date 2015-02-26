7e65d07eeb8a7cbf27245a975a112a9e30757b65
Terence <terenzio@gmail.com>
2015-02-26 13:16:18 +0800

    can calculate Collocations from ONE file! (use input3.txt)

--- PhraseSuffixTree_Terence/src/ntu/selab/phrase/suffixtree/Collocation.java
+++ PhraseSuffixTree_Terence/src/ntu/selab/phrase/suffixtree/Collocation.java
@@ -7,9 +7,10 @@ import com.aliasi.util.Files;
 import com.aliasi.util.ScoredObject;
 import com.aliasi.util.AbstractExternalizable;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
-
 import java.util.SortedSet;
 
 public class Collocation {
@@ -36,12 +37,31 @@ public class Collocation {
                             ngram);
         System.out.println("Training on "+directory);
                     
-        for (int j = 0; j < trainingFiles.length; ++j) {
-            String text = Files.readFromFile(new File(directory,
-                                                      trainingFiles[j]),
-                                             "ISO-8859-1");
-            model.handle(text);
-        }
+//        for (int j = 0; j < trainingFiles.length; ++j) {
+//            String text = Files.readFromFile(new File(directory,
+//                                                      trainingFiles[j]),
+//                                                      "ISO-8859-1");
+//            System.out.println("TEXT no:"+ j + "text: " + text);
+//            model.handle(text);
+//        }
+        
+        File file2 = new File("input3.txt");
+		try{
+			BufferedReader br = new BufferedReader(new FileReader(file2));
+			String line;
+				while((line=br.readLine()) != null){
+					//sc.scan(line);
+					//sc.scanToMix(line, 3);
+				//	  System.out.println( "Line: " + line);
+					  model.handle(line);
+				}
+	//	sc.printM();
+		br.close();
+		} catch(IOException e){
+			e.printStackTrace();
+		}
+        
+        
         return model;
     }
 
